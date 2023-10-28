c04e8f6ba67005f79b475e6d606f05066f236572
alberto <alberto.delgado@utp.ac.pa>
2023-10-28 02:55:34 -0500

    cambios

--- Laboratorio 9/laboratorio9.js
+++ Laboratorio 9/laboratorio9.js
@@ -7,21 +7,36 @@ const fila4 = document.querySelector('.fila4');
 // ----------------------------------------
 
 // Functions
-const createCards = (quantity) => {
-    let x = 1;
-    let y=0;
-    let cards = '';
-    do {
-      let row = document.querySelector(`.fila${x}`);
-      let square = '';
-      for (let i = 0; i < x; i++) {
-        square += `<div class="caja">${y+1}</div>`;
-        y=y+1;
-      }
-      row.innerHTML = square;
-      x++;
-    } while (x <= quantity);
+
+  const createCards = (quantity) => {
+    if(quantity > 1 && quantity < 5) {
+      let x = 1;
+      let y = 1;
+      do {
+        let row = document.querySelector(`.fila${x}`);
+        let square = '';
+        for (let i = 0; i < x; i++) {
+          square += `<div class="caja">${y}</div>`;
+          y++;
+        }
+        row.innerHTML = square;
+        x++;
+      } while (x <= quantity);
+    }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // ----------------------------------------
