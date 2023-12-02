0b168ec1a51e877fe66f5e64b22ef4209a616b5a
alberto <alberto.delgado@utp.ac.pa>
2023-12-01 19:37:41 -0500

    lab

---
+++ Laboratorio 11/sparkjava/src/main/java/com/alberto/App.java
@@ -0,0 +1,12 @@
+package com.alberto;
+
+/**
+ * Hello world!
+ */
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World spark  jijiji");
+    }
+}
